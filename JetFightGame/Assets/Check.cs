21614939a887fdfa25abfc1c00f35a2f07958e10
N1KL4SDEVELOPER <49870171+N1KL4SDEVELOPER@users.noreply.github.com>
2019-04-30 18:56:16 +0200

    Add Project

---
+++ JetFightGame/Assets/Check.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Aeroplane;
+
+public class Check : MonoBehaviour
+{
+
+    // <>
+
+    public GameObject Cam;
+    PhotonView pView;
+
+    void Start()
+    {
+        pView = GetComponent<PhotonView>();
+        if(pView.isMine)
+        {
+          
+        }
+        else
+        {
+            Cam.SetActive(false);
+            GetComponent<AeroplaneController>().enabled = false;
+        }
+
+    }
+}
---
+++ JetFightGame/Assets/Manager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    // <>
+
+
+    public Transform Spawn;
+    public bool connected;
+    public GameObject Player;
+
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("v1");
+    }
+    private void OnConnectedToMaster()
+    {
+        Debug.Log("CONNECTED TO MASTER");
+        PhotonNetwork.JoinLobby();
+    }
+    void OnJoinedLobby()
+    {
+        Debug.Log("CONNECTED TO LOBBY");
+        PhotonNetwork.JoinRandomRoom();
+    }
+    void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("RANDOM JOIN FAILED");
+        PhotonNetwork.CreateRoom(null);
+    }
+    void OnJoinedRoom()
+    {
+        connected = true;      
+        Player = PhotonNetwork.Instantiate("myPlayer", Spawn.position, Quaternion.identity, 0);     
+    }
+}
